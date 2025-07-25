87eb9b0bf7571af42d8f30afcbfcf586edb5b7d6
alexandra andr <shura_1986@mail.ru>
2025-07-25 19:36:02 +0300

    writing function search by phone numbers

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from config import PATH_XLSX
+from src.services import get_simple_search
+from src.utils import get_cards
 from src.views import get_main_page_info
 
+def main():
+    main_page_result = get_main_page_info("24.12.2021 15:44:07")
+    print(main_page_result)
 
-main_page_result = get_main_page_info("2021-12-11 01:02:03")
-print(main_page_result)
+    all_transactions = get_cards(PATH_XLSX)
+    transactions_for_service = all_transactions.to_dict("records")
+    service_resalt = get_simple_search(transactions_for_service, "Перевод")
+    print(service_resalt)
+
+if __name__ == '__main__':
+    main()
---
+++ src/services.py
@@ -0,0 +1,41 @@
+import json
+import re
+
+from logger import logger
+
+
+def get_simple_search(transactions, keyword):
+    """
+    Функция возвращает JSON со всеми транзакциями, которые:
+    1. Относятся к указанной категории (keyword)
+    2. В описании содержат имя и инициал фамилии (например, "Иван С.")
+    Аргументы:
+        transactions: Путь к файлу с транзакциями
+        keyword: Ключевое слово для фильтрации категории
+    """
+
+    # pd_transactions = pd.read_excel(transactions)
+    # list_transactions = pd_transactions.to_dict("records")
+    logger.info(f"Начало фильтрации транзакций. Категория: '{keyword}'")
+    logger.debug(f"Получено {len(transactions)} транзакций для обработки")
+
+    # Компиляция регулярного выражения для поиска "Имя Ф."
+    logger.debug("Компиляция регулярного выражения для поиска: Имя Ф.")
+    pattern = re.compile(r"\b[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.", flags=re.IGNORECASE)
+    logger.info("Регулярное выражение успешно скомпилировано")
+
+    # Фильтрация транзакций
+    filter_by_category = []
+    logger.debug("Начало обработки транзакций...")
+    for category in transactions:
+       if keyword.lower() in str(category.get("Категория", "")).lower() and pattern.search(str(category.get("Описание", ""))):
+            filter_by_category.append(category)
+
+    logger.info(f"Найдено {len(filter_by_category)} подходящих транзакций")
+    logger.debug(f"Список отфильтрованных транзакций {json.dumps(filter_by_category, indent=2, ensure_ascii=False)}")
+    logger.info(f"Фильтрация завершена успешно.")
+    # return filter_by_category
+    return json.dumps(filter_by_category, indent=2, ensure_ascii=False)
+
+# if __name__ == '__main__':
+#     print(get_simple_search(PATH_XLSX, "Переводы"))
